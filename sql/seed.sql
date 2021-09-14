5a4085ebc73d6ae6c5bee7877922e3fa991e6153
Sarah dePalo <sarahdepalo99@gmail.com>
2021-09-14 10:57:26 -0400

    added results seed data and character images

--- sql/seed.sql
+++ sql/seed.sql
@@ -318,4 +318,53 @@ VALUES
 INSERT INTO results
     (character_img, results_text, character_id)
 VALUES
-    ('../public/images/finn.jpeg', 'Like Finn, you are brave, kind, selfless, and totally rad! You always put others before yourself and go the extra mile to make those you love happy. You have a strong sense of what is right and wrong and will stop at nothing to pursue those ideals. Despite being a bit impulsive and aggressive at times, you create deep and meaningful relationships with those around you. Mathematical!', 1)
+    ('finn.jpeg', 'Like Finn, you are brave, kind, selfless, and totally rad! You always put others before yourself and go the extra mile to make those you love happy. You have a strong sense of what is right and wrong and will stop at nothing to pursue those ideals. Despite being a bit impulsive and aggressive at times, you create deep and meaningful relationships with those around you. Mathematical!', 1);
+
+-- Jake
+
+INSERT INTO results
+    (character_img, results_text, character_id)
+VALUES
+    ('jake.jpeg', 'Like Jake, youre witty, funny, and a bit weird. You dance like no one is watching, and you can make some really good food (or at least enjoy eating good food). Despite your laid-back demeanor, you are very loyal and will protect those you love at all costs. People close to you tend to come to you for advice or when they need cheering up.', 2 );
+
+-- Marceline
+
+INSERT INTO results
+    (character_img, results_text, character_id)
+VALUES
+    ('marceline.png', 'Marceline is wild, hardcore, and a jokester, but also a good friend like you. You tend to hide your sensitive and loving side and can be hesitant to make new relationships. But when you are close with somebody, you are a great and loyal friend!', 3 );
+
+-- Ice King
+
+INSERT INTO results
+    (character_img, results_text, character_id)
+VALUES
+    ('ice_king.jpeg', '"Im a banana!" is just one of many great quotes from the Ice King who shares your kind personality and weird sense of humor. Like the Ice King, you are often misunderstood and a bit awkward, but the people around you love you anyway. Just dont adopt any pet penguins or put on any crowns and youll be good!', 4 );
+
+-- Lemongrab
+
+INSERT INTO results
+    (character_img, results_text, character_id)
+VALUES
+    ('lemongrab.jpeg', 'Lemongrab is mean, rude, and obnoxious, but ya know, he didnt ask to be born so well cut him some slack. Like Lemongrab, you are in touch with your emotions and a bit sensitive. You might get upset if things dont go your way, because thats pretty UNACCEPTAAABLLLEEEEE!!', 5);
+
+-- Princess Bubblegum
+
+INSERT INTO results
+    (character_img, results_text, character_id)
+VALUES
+    ('princess_bubblegum.jpeg', 'Like Princess Bubblegum, you are kind and compassionate. You have a very strong sense of what is right and wrong, which can sometimes cause your dark side to come out. You are very intelligent and are straddled with lots of responsibilities. So, you enjoy occasionally being able to kick back, relax, and jam out.', 6);
+
+-- Flame Princess
+
+INSERT INTO results
+    (character_img, results_text, character_id)
+VALUES
+    ('flame_princess.jpeg', 'Flame Princess is pretty chill but also has a short temper. Despite occasional mood swings, you are caring and passionate and fight for what you believe in. You dont let anyone tell you what to do, and you walk to the beat of your own drum. You''re also surprisingly good at rapping.', 7);
+
+-- Tree Trunks
+
+INSERT INTO results
+    (character_img, results_text, character_id)
+VALUES
+    ('tree_trunks.jpeg', 'Tree Trunks is gentle, slow, and classy, but quite naive. Like Tree Trunks, you are polite, passionate, and can make a mean apple pie. A romantic at heart, you work hard to show those around you that you care about them.', 8);
